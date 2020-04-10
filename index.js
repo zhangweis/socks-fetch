b9964bb2c3ab967e73fde9bd36c98ff6f6ee98a4
zhangwei <cdzhangw@gmail.com>
2020-04-10 18:53:43 +0800

    agents()

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var HTTPsAgent = require('socks5-https-client/lib/Agent')
 var HTTPAgent = require('socks5-http-client/lib/Agent')
 var caller = require('caller');
 const path = require('path');
-
-module.exports = function(url, options) {
+module.exports.agents = function () {
+  var c = caller();
+  var engineName =path.basename(c, path.extname(c));
+  var socksHost = (process.env.socks_host || process.env['socks_host_'+engineName]);
+  var socksPort = (process.env.socks_port || process.env['socks_port_'+engineName]);
+var httpsAgent = new HTTPsAgent({socksHost:socksHost,socksPort})
+var httpAgent = new HTTPAgent({socksHost:socksHost,socksPort})
+  return {httpsAgent,httpAgent};
+}
+module.exports.fetch = function(url, options) {
   options = options || {};
   var c = caller();
   var engineName =path.basename(c, path.extname(c));
